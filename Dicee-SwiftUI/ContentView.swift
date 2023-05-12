d0a5f399e6abb6336594a99dd40ac2cfde7f2511
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-05-12 16:28:25 +0300

    1

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -1,21 +1,55 @@
-//
-//  ContentView.swift
-//  Dicee-SwiftUI
-//
-//  Created by Максим Сиверцев on 12.05.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 3
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack (
+                alignment: .center,
+                spacing: 150
+            ) {
+                Image("diceeLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                HStack (
+                    spacing: 20
+                ) {
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Button("ROLL")
+                {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }
+                .font(Font.system(size: 50))
+                .fontWeight(.bold)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 70)
+                .background(Color(UIColor(red: 0.84, green: 0.19, blue: 0.19, alpha: 1)))
+                .cornerRadius(50)
+                .foregroundColor(.white)
+            }
         }
-        .padding()
+    }
+}
+
+struct DiceView: View {
+    let number: Int
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(.all, 5)
     }
 }
 
@@ -24,3 +58,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
